c58ff381d9dc33ad13a5907b053957824b6fdf77
Grant Shipley <gshipley@gmail.com>
2013-05-16 16:09:25 -0400

    changed connection

--- src/main/java/org/openshift/haproxy/GearParser.java
+++ src/main/java/org/openshift/haproxy/GearParser.java
@@ -22,7 +22,7 @@ public class GearParser {
 					String[] splitLines = line.split(",");
 					Gear currentGear = new Gear(splitLines[1], splitLines[0],
 							new Integer(splitLines[9]), new Integer(
-									splitLines[7]));
+									splitLines[4]));
 					gears.add(currentGear);
 					System.out.println("Added new gear to arraylist");
 				}
