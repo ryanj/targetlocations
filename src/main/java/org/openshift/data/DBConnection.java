2584553097174000041b14a4410200b58a79a56f
Grant Shipley <gshipley@gmail.com>
2013-05-16 16:40:10 -0400

    fix my stupid bug

--- src/main/java/org/openshift/data/DBConnection.java
+++ src/main/java/org/openshift/data/DBConnection.java
@@ -27,7 +27,7 @@ public class DBConnection {
 	public void afterCreate() {
 		System.out.println("just see if we can say anything");
 		
-		String host = System.getenv("OPENSHIFT_MONGODB_DB_HOST");
+		String host = System.getenv("OPENSHIFT_MONGODB_DB_GEAR_DNS");
 		
 		if (host == null || "".equals(host)){
 			//we are not on openshift
